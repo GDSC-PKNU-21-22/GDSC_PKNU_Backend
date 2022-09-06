7803474510051ab9c718d4dddaf8b502ed40c3c3
JoHyeonseok <wh9832@naver.com>
2022-09-07 02:20:00 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/gdscpknu/gdscpknu/GdscpknuApplication.java
@@ -0,0 +1,13 @@
+package com.gdscpknu.gdscpknu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdscpknuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdscpknuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdscpknu/gdscpknu/GdscpknuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdscpknu.gdscpknu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdscpknuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
