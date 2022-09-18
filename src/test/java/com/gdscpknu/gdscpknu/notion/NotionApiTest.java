e77e4808b7e1c0dd2c5b6be765d2a9567d65fde4
JoHyeonseok <wh9832@naver.com>
2022-09-19 03:44:39 +0900

    test: #3 Notion 테이블 plainText 추출

--- src/test/java/com/gdscpknu/gdscpknu/notion/NotionApiTest.java
+++ src/test/java/com/gdscpknu/gdscpknu/notion/NotionApiTest.java
@@ -10,6 +10,11 @@ import org.jraf.klibnotion.model.pagination.Pagination;
 import org.jraf.klibnotion.model.pagination.ResultPage;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class NotionApiTest {
@@ -63,6 +68,41 @@ public class NotionApiTest {
         assertThat(simpleQueryResultPage.results.get(0).getPropertyValues().toString().contains(email)).isEqualTo(true);
         assertThat(simpleQueryResultPage.results.get(0).getPropertyValues().toString().contains(role)).isEqualTo(true);
         assertThat(simpleQueryResultPage.results.get(0).getPropertyValues().toString().contains(name)).isEqualTo(true);
+    }
+
+    @Test
+    void getPlainTexts(){
+
+        //given
+        String name = "가나다";
+        String role = "Core";
+        String email = "ganada@pukyong.ac.kr";
+
+        ResultPage<Page> simpleQueryResultPage = client.getDatabases().queryDatabase(
+                notionTable.getDATABASE_ID(),
+                null,
+                null,
+                new Pagination()
+        );
+
+        //when
+        /**
+         * KlibNotion 라이브러리에서 개별 plainText를 받아올 수 없어 정규식으로 추출하였음
+         */
+        String resultInString = simpleQueryResultPage.results.toString();
+        List<String> plainTextList = new ArrayList<>();
+        // 문자열 중 "(plainText=" ~ "," 범위(이름, 이메일) OR " "name=" ~ "," 범위(역할) 문자열을 추출
+        Pattern pattern = Pattern.compile("(?<=\\(plainText=)(.*?)(?=,)|(?<=\\(name=)(.*?)(?=,)");
+        Matcher matcher = pattern.matcher(resultInString);
+        while (matcher.find()) {
+            plainTextList.add(matcher.group());
+        }
+
+        //then
+        assertThat(plainTextList.size()).isEqualTo(notionTable.getMemberNum() * 3);
+        assertThat(plainTextList.get(0)).isEqualTo(email);
+        assertThat(plainTextList.get(1)).isEqualTo(role);
+        assertThat(plainTextList.get(2)).isEqualTo(name);
     }
 
 }
